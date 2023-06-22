e02b94c9513a49d611cbe4ad484a1588df2032eb
Ben Line <hello@benline.co.uk>
2023-06-22 21:24:04 +0000

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,7 @@ then
   echo "I made this custom edit"
   echo [SGA] [$(date '+%d-%m-%Y %H:%M:%S')] Automated changes
   gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
-               -b [SGA] [$(date '+%d-%m-%Y %H:%M:%S')] Changes from main repo \
+               -b [$INPUT_SYMBOL] [$(date '+%d-%m-%Y %H:%M:%S')] Changes from main \
                -B $INPUT_DESTINATION_BASE_BRANCH \
                -H $INPUT_DESTINATION_HEAD_BRANCH \
                   $PULL_REQUEST_REVIEWERS
