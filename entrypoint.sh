eba99a51975b61b9409c10017414a8c10966e357
Ben Line <hello@benline.co.uk>
2023-07-12 02:18:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,7 @@ git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$C
 
 echo "Copying contents to git repo"
 mkdir -p "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
-cp -R "$INPUT_SOURCE_FOLDER" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
+cp -R $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
 git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
@@ -57,10 +57,4 @@ then
   git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
   echo "Creating a pull request"
   gh pr create -t "[$INPUT_SYMBOL] [$(date '+%d-%m-%Y %H:%M:%S')] $INPUT_MESSAGE" \
-               -b "[$INPUT_SYMBOL] - Beep Boop - Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA" \
-               -B "$INPUT_DESTINATION_BASE_BRANCH" \
-               -H "$INPUT_DESTINATION_HEAD_BRANCH" \
-               $PULL_REQUEST_REVIEWERS
-else
-  echo "No changes detected"
-fi
+               -b "[$INPUT_SYMBOL] - Beep Boop - Update from https://github.com/$GITHUB_REPOSITORY
