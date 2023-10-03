ddd68cc6d06ae209496f4fd4aeab50d06529e006
Lino <hello@benline.co.uk>
2023-10-03 18:56:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,17 +51,17 @@ done
 
 git status
 
-# if git status | grep -q "Changes to be committed"
-# then
-#   git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-#   echo "Pushing git commit"
-#   git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
-#   echo "Creating a pull request"
-#   gh pr create -t "[$INPUT_SYMBOL] [$(date '+%d-%m-%Y %H:%M:%S')] $INPUT_MESSAGE" \
-#                -b "$INPUT_BODY"$'\n\n\n'"From: https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA" \
-#                -B $INPUT_DESTINATION_BASE_BRANCH \
-#                -H $INPUT_DESTINATION_HEAD_BRANCH \
-#                   $PULL_REQUEST_REVIEWERS
-# else
-#   echo "No changes detected"
-# fi
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+  echo "Pushing git commit"
+  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+  echo "Creating a pull request"
+  gh pr create -t "[$INPUT_SYMBOL] [$(date '+%d-%m-%Y %H:%M:%S')] $INPUT_MESSAGE" \
+               -b "$INPUT_BODY"$'\n\n\n'"From: https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA" \
+               -B $INPUT_DESTINATION_BASE_BRANCH \
+               -H $INPUT_DESTINATION_HEAD_BRANCH \
+                  $PULL_REQUEST_REVIEWERS
+else
+  echo "No changes detected"
+fi
