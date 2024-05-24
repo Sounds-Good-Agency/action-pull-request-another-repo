f134a4cc65445bfe62f540fd74e62cb9c4700d8c
Ahsan Javed <ahsan@soundsgood.agency>
2024-05-24 12:49:31 +0500

    putting echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,11 +43,18 @@ echo "Cloning destination git repository"
 echo "Cloning to $INPUT_DESTINATION_BASE_BRANCH"
 git clone -b $INPUT_DESTINATION_BASE_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
+echo "CHECKING STATUS BEFORE COPYING %%%%%%%%%%%%%%%%%%%%%%%%%%"
+git status
+
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
 cp -R $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cp -R ./.[!.]* "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
+
+echo "CHECKING STATUS BEFORE SWITCHING %%%%%%%%%%%%%%%%%%%%%%%%%%"
+git status
+
 git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
 echo "$INPUT_BODY"
