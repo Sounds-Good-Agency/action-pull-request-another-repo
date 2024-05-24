e8399b73313aa08e77b646a5cccaa9724a4c28a1
Ahsan Javed <ahsan@soundsgood.agency>
2024-05-24 10:33:53 +0500

    adding missing files into array

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,18 +68,18 @@ missing_files=""
 
 # Loop through each file in the input list
 for file in $INPUT_DESTINATION_FILES; do
-  if [! -f "$file" ]; then
+  if [! -f $file ]; then
     # File does not exist, append its name to the missing_files string
-    missing_files="$missing_files$file "
+    missing_files=$missing_files$file
   else
-    git add "$file"
+    git add $file
   fi
 done
 
 # Check if there are any missing files and echo them
-if [ -n "$missing_files" ]; then
+if [ -n $missing_files ]; then
   echo "The following files do not exist:"
-  echo "$missing_files"
+  echo $missing_files
 else
   echo "All files exist and have been added to Git."
 fi
